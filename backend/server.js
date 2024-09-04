1f575e1cd1e0d23634044aa14ed48c5dd7b89641
Husnam ilyas <husnamilyas89@gmail.com>
2024-09-04 19:27:45 +0500

    testing

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,21 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use('/api/users', userRoutes);
 
-// if (process.env.NODE_ENV === 'production'){
-//     const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production'){
+    // const __dirname = path.resolve();
+    // app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    // app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+
+    const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
-// }else{
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+
+}else{
 
     app.get('/', (req, res) => res.send('Server is ready'));
-// }
+}
 
 
 app.use(notFound);
